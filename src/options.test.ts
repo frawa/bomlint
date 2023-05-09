b6a9286f1d668164e3693441af2a07168b38b69a
Frank Wagner <frank.wagner@fr.ibm.com>
2023-05-09 10:34:33 +0200

    testable commands parsing

--- src/options.test.ts
+++ src/options.test.ts
@@ -1,73 +1,113 @@
-import { parseOptions } from "./options";
+import { Command, parseOptions, parseOptionsWithOutput } from "./options";
 
 describe('options', function () {
 
     const nodeArgs = (args: string[]) => ['node', 'script', ...args]
 
-    describe('check command', () => {
-        it("nothing", () => {
-            const command = parseOptions(nodeArgs([]))
-            expect(command).toEqual(({ command: 'check', files: [] }))
+    describe('help', () => {
+        it("global help", () => {
+            const [command,output] = parseOptionsWithOutput(nodeArgs(['--help']))
+            expect(output).toMatch(/^Usage: bomlint/)
         });
-        it("help", () => {
-            const command = parseOptions(nodeArgs(['--help']))
-            expect(command).toMatch(/^Usage: bomlint/)
+        it("global help command", () => {
+            const [command,output] = parseOptionsWithOutput(nodeArgs(['help']))
+            expect(output).toMatch(/^Usage: bomlint/)
+        });
+        it("check help", () => {
+            const [command,output] = parseOptionsWithOutput(nodeArgs(['help', 'check']))
+            expect(output).toMatch(/^Usage: bomlint check/)
         });
         it("unkown", () => {
-            const command = parseOptions(nodeArgs(['--boom']))
-            expect(command).toMatch(/^Usage: bomlint/)
+            const [command,output] = parseOptionsWithOutput(nodeArgs(['--boom']))
+            expect(output).toMatch(/^error: unknown option '--boom'/)
+            expect(output).toMatch(/Usage: bomlint/)
+        });
+        it("nothing", () => {
+            const [command,output] = parseOptionsWithOutput(nodeArgs([]))
+            expect(output).toMatch(/^Usage: bomlint/)
+        });
+    });
+
+    describe('check command', () => {
+        it("no file", () => {
+            const command = parseOptions(nodeArgs(['check']))
+            expect(command).toEqual<Command>({ command: 'check', files: [] })
         });
         it("just a file", () => {
-            const command = parseOptions(nodeArgs(['file.txt']))
+            const command = parseOptions(nodeArgs(['check', 'file.txt']))
             expect(command).toEqual({ command: 'check', files: ['file.txt'] })
         });
-        it("check two files", () => {
-            const command = parseOptions(nodeArgs(['file.txt', 'file2.txt']))
+        it("two files", () => {
+            const command = parseOptions(nodeArgs(['check', 'file.txt', 'file2.txt']))
             expect(command).toEqual({ command: 'check', files: ['file.txt', 'file2.txt'] })
+        });
+        it("with bom file", () => {
+            const command = parseOptions(nodeArgs(['check', '--bom', 'bomfile', 'file.txt', 'file2.txt']))
+            expect(command).toEqual({ command: 'check', bom: 'bomfile', files: ['file.txt', 'file2.txt'] })
+        });
+        it("allow conflicts", () => {
+            const command = parseOptions(nodeArgs(['check', '--allow-conflicts', 'allow1,allow2', 'file.txt']))
+            expect(command).toEqual<Command>({
+                command: 'check',
+                allowConflicts: ['allow1', 'allow2'], 
+                files: ['file.txt']
+            })
         });
     });
 
     describe('fix command', () => {
         it("nothing", () => {
-            const command = parseOptions(nodeArgs(['--fix']))
+            const command = parseOptions(nodeArgs(['fix']))
             expect(command).toEqual(({ command: 'fix', files: [] }))
         });
         it("just a file", () => {
-            const command = parseOptions(nodeArgs(['--fix', 'file.txt']))
+            const command = parseOptions(nodeArgs(['fix', 'file.txt']))
             expect(command).toEqual({ command: 'fix', files: ['file.txt'] })
         });
-        it("check two files", () => {
-            const command = parseOptions(nodeArgs(['--fix', 'file.txt', 'file2.txt']))
-            expect(command).toEqual({ command: 'fix', files: ['file.txt', 'file2.txt'] })
+        it("allow conflicts", () => {
+            const command = parseOptions(nodeArgs(['fix', '--allow-conflicts', 'allow1,allow2', 'file.txt']))
+            expect(command).toEqual({
+                command: 'fix',
+                allowConflicts: ['allow1', 'allow2'], 
+                files: ['file.txt']
+            })
         });
     });
 
     describe('merge command', () => {
         it("nothing", () => {
-            const command = parseOptions(nodeArgs(['--merge']))
+            const command = parseOptions(nodeArgs(['merge']))
             expect(command).toEqual(({ command: 'merge', files: [] }))
         });
         it("just a file", () => {
-            const command = parseOptions(nodeArgs(['--merge', '--bom', 'bom.file', 'file.txt']))
+            const command = parseOptions(nodeArgs(['merge', '--bom', 'bom.file', 'file.txt']))
             expect(command).toEqual({ command: 'merge', bom: 'bom.file', files: ['file.txt'] })
         });
         it("check two files", () => {
-            const command = parseOptions(nodeArgs(['--merge', '--bom', 'bom.file', 'file.txt', 'file2.txt']))
+            const command = parseOptions(nodeArgs(['merge', '--bom', 'bom.file', 'file.txt', 'file2.txt']))
             expect(command).toEqual({ command: 'merge', bom: 'bom.file', files: ['file.txt', 'file2.txt'] })
+        });
+        it("allow conflicts", () => {
+            const command = parseOptions(nodeArgs(['merge', '--allow-conflicts', 'allow1,allow2', 'file.txt']))
+            expect(command).toEqual({
+                command: 'merge',
+                allowConflicts: ['allow1', 'allow2'], 
+                files: ['file.txt']
+            })
         });
     });
 
     describe('prune command', () => {
         it("nothing", () => {
-            const command = parseOptions(nodeArgs(['--prune']))
+            const command = parseOptions(nodeArgs(['prune']))
             expect(command).toEqual(({ command: 'prune', files: [] }))
         });
         it("just a file", () => {
-            const command = parseOptions(nodeArgs(['--prune', '--bom', 'bom.file', 'file.txt']))
+            const command = parseOptions(nodeArgs(['prune', '--bom', 'bom.file', 'file.txt']))
             expect(command).toEqual({ command: 'prune', bom: 'bom.file', files: ['file.txt'] })
         });
         it("check two files", () => {
-            const command = parseOptions(nodeArgs(['--prune', '--bom', 'bom.file', 'file.txt', 'file2.txt']))
+            const command = parseOptions(nodeArgs(['prune', '--bom', 'bom.file', 'file.txt', 'file2.txt']))
             expect(command).toEqual({ command: 'prune', bom: 'bom.file', files: ['file.txt', 'file2.txt'] })
         });
     });
--- src/options.ts
+++ src/options.ts
@@ -1,50 +1,96 @@
-import { ParseOptions, createCommand } from "commander";
+import { ParseOptions, createCommand, Command as CommanderCommand } from "commander";
 const myPackageJson = require("../package.json");
 
-export type Command = { command: 'check', files: string[] }
-    | { command: 'fix', files: string[] }
-    | { command: 'merge', files: string[], bom?: string }
+export type Command = { command: 'check', files: string[], bom?: string, allowConflicts?: string[] }
+    | { command: 'fix', files: string[], bom?: string, allowConflicts?: string[] }
+    | { command: 'merge', files: string[], bom?: string, allowConflicts?: string[] }
     | { command: 'prune', files: string[], bom?: string };
 
-export type Help = string
+export function parseOptions(args: readonly string[]): Command | undefined {
+    return parseOptions_(args)
+}
 
+export function parseOptionsWithOutput(args: readonly string[]): [Command | undefined, string] {
+    const buffer: string[] = []
+    const command = parseOptions_(args, (text: string) => buffer.push(text))
+    return [command, buffer.join("\n")]
+}
 
+function parseOptions_(args: readonly string[], writeOut?: (text: string) => void): Command | undefined {
+    const configure = (command: CommanderCommand) => {
+        command.exitOverride()
+        command.showHelpAfterError(true)
+        writeOut && command.configureOutput({ writeOut, writeErr: writeOut })
+        return command
+    }
+    const checkCommand = configure(
+        createCommand('check')
+            .description("Checks package dependencies against BOM")
+            .option("--allow-conflicts <dependencies>]", "Allow conflicts for the dependencies (comma-separated)")
+            .argument("[<file...>]", "package.json file(s) to be processed", "package.json")
+    )
 
-export function parseOptions(args: readonly string[]): Command | Help {
-    const program1 = createCommand()
-    program1
-        .name("bomlint")
-        .version(myPackageJson.version)
-        .description("Checks package dependencies against BOM")
-        .option("--allow-conflicts <dependencies>]", "Allow conflicts for the dependencies (comma-separated)")
-        .option("--fix", "Apply bom file to package dependencies")
-        .option("--merge", "Add package dependencies to BOM file")
-        .option("--prune", "Remove redundant dependencies from BOM file")
-        .option("--bom <bomfile>", "Path to BOM file")
-        .argument("[<file...>]", "package.json file(s) to be checked/fixed", "package.json")
-        .action(args => {
-            console.log('FW args', args)
-        })
-        // .exitOverride((err) => console.log('FW err', err));
-        .exitOverride();
+    const fixCommand = configure(
+        createCommand('fix')
+            .description("Fixes package dependencies against BOM")
+            .option("--allow-conflicts <dependencies>]", "Allow conflicts for the dependencies (comma-separated)")
+            .argument("[<file...>]", "package.json file(s) to be processed", "package.json")
+    )
+
+    const mergeCommand = configure(
+        createCommand('merge')
+            .description("Merges package dependencies into BOM")
+            .option("--allow-conflicts <dependencies>]", "Allow conflicts for the dependencies (comma-separated)")
+            .argument("[<file...>]", "package.json file(s) to be processed", "package.json")
+    )
+
+    const pruneCommand = configure(
+        createCommand('prune')
+            .description("Prune package dependencies from BOM")
+            .argument("[<file...>]", "package.json file(s) to be processed", "package.json")
+    )
+
+    const program1 = configure(
+        createCommand()
+            .name("bomlint")
+            .version(myPackageJson.version)
+            .description("Align package dependencies with Bill of Materials (BOM).")
+            .option("--bom <bomfile>", "Path to BOM file")
+            .addCommand(checkCommand)
+            .addCommand(fixCommand)
+            .addCommand(mergeCommand)
+            .addCommand(pruneCommand)
+    )
+
     try {
         program1.parse(args)
     } catch (err) {
-        // console.log('FW err2', err)
-        // program1.outputHelp() 
-        return program1.helpInformation();
+        return undefined;
     }
-    const opts1 = program1.opts()
-    const args1 = program1.args
-    console.log('FW', args, opts1, args1)
+    
+    const command = program1.args[0]
+    const bom: string = program1.opts().bom
 
-    if (opts1.fix) {
-        return { command: 'fix', files: args1 };
-    } else if (opts1.merge) {
-        return { command: 'merge', bom: opts1.bom, files: args1 };
-    } else if (opts1.prune) {
-        return { command: 'prune', bom: opts1.bom, files: args1 };
+    switch (command) {
+        case 'check': {
+            const files = checkCommand.args
+            const allowConflicts: string[] = checkCommand.opts().allowConflicts?.split(",")
+            return { command: 'check', files, bom, allowConflicts };
+        }
+        case 'fix': {
+            const files = fixCommand.args
+            const allowConflicts = fixCommand.opts().allowConflicts?.split(",")
+            return { command: 'fix', files, bom, allowConflicts };
+        }
+        case 'merge': {
+            const files = mergeCommand.args
+            const allowConflicts = mergeCommand.opts().allowConflicts?.split(",")
+            return { command: 'merge', files, bom, allowConflicts };
+        }
+        case 'prune': {
+            const files = pruneCommand.args
+            return { command: 'prune', files, bom };
+        }
     }
-
-    return { command: 'check', files: args1 };
+    return undefined;
 }
